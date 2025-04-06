90897dd202bf8fd219a03e4a055e5d864ec83d59
Akihiro <kevin2006phan@gmail.com>
2025-04-07 09:46:00 +1000

    changed resolution

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -10,7 +10,7 @@ Game::Game() {
 
 	// Initialize terminal
 	terminal_open();
-	terminal_set("window: size=160x50, title='Aethereal', resizable=true; font: ../resources/fonts/fixedsys62.ttf, size=24, hinting=normal;");
+	terminal_set("window: size=160x50, title='Aethereal', resizable=true; font: ../resources/fonts/fixedsys62.ttf, size=18, hinting=normal;");
 
 	// initialize
 	ServiceLocator::provide(std::make_shared<InputManager>());
