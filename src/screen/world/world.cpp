37b0a8e4bce550bd0bf88c700a97a95cfd56e75c
Akihiro <kevin2006phan@gmail.com>
2025-04-07 01:34:35 +1000

    updated world menu

--- src/screen/world/world.cpp
+++ src/screen/world/world.cpp
@@ -2,11 +2,14 @@
 #include "../utilities/draw_utilities.h"
 
 void World::render() {
-	
-	terminal_color("white");
-	DrawUtils::draw_box_heavy({ 0, 0, 32, 16 });
+	// screen dimensions
+	int screen_width = terminal_state(TK_WIDTH)-1;
+	int screen_height = terminal_state(TK_HEIGHT)-1;
+
+	m_game_panel.render(DrawUtils::Box(0.0f, 0.0f, screen_width*0.75f-1, (float)screen_height));
+	m_status_panel.render(DrawUtils::Box(screen_width*0.75f, 0.0f, screen_width*0.25f, screen_height*0.75f-1));
+	m_message_panel.render(DrawUtils::Box(screen_width*0.75f, screen_height*0.75f, screen_width*0.25f, screen_height*0.25f));
 }
 
 void World::update() {
-
 }
--- src/screen/world/world.h
+++ src/screen/world/world.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "../screen.h"
+#include "game_panel.h"
+#include "status_panel.h"
+#include "message_panel.h"
 
 class World : public Screen {
 public:
@@ -7,4 +10,9 @@ public:
 
 	void render() override;
 	void update() override;
+
+private:
+	GamePanel m_game_panel;
+	StatusPanel m_status_panel;
+	MessagePanel m_message_panel;
 };
