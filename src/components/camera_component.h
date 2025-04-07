db4d6d29f6228fb307a4ab373d2e4ef443f7a65a
Akihiro <kevin2006phan@gmail.com>
2025-04-08 02:25:38 +1000

    added camera system and component

---
+++ src/components/camera_component.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct Camera {
+	float tracking_x = 0.0f;
+	float tracking_y = 0.0f;
+};
---
+++ src/screen/world/systems/camera_system.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <fecs.h>
+#include <BearLibTerminal.h>
+#include <glm/glm.hpp>
+#include "../../../services/service_locator.h"
+#include "../../../components/camera_component.h"
+#include "../../../components/player_component.h"
+#include "../../../components/position_component.h"
+
+class CameraSystem {
+public:
+	CameraSystem() = default;
+
+	void update() {
+		// Update the camera position based on the entity's position
+		auto ecs = ServiceLocator::get_service<FECS>();
+		ecs->query<Camera>([&ecs](Entity entity, Camera& camera) {
+			
+			// track player
+			ecs->query<Player, Position>([&camera](Entity e, Player& _, Position& epos) {
+
+				/*std::cout << "Camera tracking player at: " << epos.x << ", " << epos.y << std::endl;*/
+				camera.tracking_x = glm::floor(epos.x - (terminal_state(TK_WIDTH) * 0.75f * 0.5f));
+				camera.tracking_y = glm::floor(epos.y - (terminal_state(TK_HEIGHT) * 0.75f * 0.5f)) - 3.0f;
+			});
+		});
+	}
+};
