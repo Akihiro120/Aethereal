ce42b375532b81a05bf326878ab63b4aea1801c5
Akihiro <kevin2006phan@gmail.com>
2025-04-07 18:52:34 +1000

    added systems to world

--- src/screen/world/game_panel.cpp
+++ src/screen/world/game_panel.cpp
@@ -2,12 +2,22 @@
 
 #include "../utilities/draw_utilities.h"
 
+GamePanel::GamePanel() {
+	// initialize game panel
+	m_world_system.generate_world();
+}
+
 void GamePanel::render(const DrawUtils::Box& box) {
 	// game panel
 	terminal_color("white");
 	DrawUtils::draw_box_heavy(box);
+
+	// render within bounds of the screen
+	m_world_system.render(box);
+	m_render_system.render(box);
 }
 
 void GamePanel::update() {
 	// update game panel
+	m_world_system.update();
 }
--- src/screen/world/game_panel.h
+++ src/screen/world/game_panel.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "../utilities/draw_utilities.h"
+#include "systems/render_systems.h"
+#include "systems/world_system.h"
 
 class GamePanel {
 public:
-	GamePanel() = default;
+	GamePanel();
 
 	void render(const DrawUtils::Box& box);
 	void update();
+
+private:
+	WorldSystem m_world_system;
+	RenderSystem m_render_system;
 };
