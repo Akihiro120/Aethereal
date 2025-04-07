34da7dee69b2bc30e940db3d0ea7d81142b05131
Akihiro <kevin2006phan@gmail.com>
2025-04-07 18:53:15 +1000

    abstract information to panels

--- src/screen/world/world.cpp
+++ src/screen/world/world.cpp
@@ -1,15 +1,40 @@
 #include "world.h"
 #include "../utilities/draw_utilities.h"
+#include "../../input/input_manager.h"
+
+World::World() {
+	init();
+}
 
 void World::render() {
+
 	// screen dimensions
 	int screen_width = terminal_state(TK_WIDTH)-1;
 	int screen_height = terminal_state(TK_HEIGHT)-1;
 
+	// render the panels
 	m_game_panel.render(DrawUtils::Box(0.0f, 0.0f, screen_width*0.75f-1, (float)screen_height));
 	m_status_panel.render(DrawUtils::Box(screen_width*0.75f, 0.0f, screen_width*0.25f, screen_height*0.75f-1));
 	m_message_panel.render(DrawUtils::Box(screen_width*0.75f, screen_height*0.75f, screen_width*0.25f, screen_height*0.25f));
 }
 
+void World::init() {
+	// initialize player
+	auto ecs = ServiceLocator::get_service<FECS>();
+	Entity player = ecs->create_entity();	
+	ecs->attach<Position>(player, Position(0.0f, 0.0f));
+	ecs->attach<Velocity>(player, Velocity(0.0f, 0.0f));
+	ecs->attach<Render>(player, Render{(int)'@', 0xFF00FF00});
+	ecs->attach<Player>(player, Player{});
+}
+
 void World::update() {
+
+	auto input = ServiceLocator::get_service<InputManager>();
+	input->process_input([this](int key) {
+
+		// process input for player movement
+		m_player_system.movement(key);
+	});
+	m_physics_system.resolve_velocities();
 }
--- src/screen/world/world.h
+++ src/screen/world/world.h
@@ -3,16 +3,25 @@
 #include "game_panel.h"
 #include "status_panel.h"
 #include "message_panel.h"
+#include "systems/player_systems.h"
+#include "systems/physics_system.h"
 
 class World : public Screen {
 public:
-	World() = default;
+	World();
 
 	void render() override;
 	void update() override;
 
 private:
+	void init();
+	
+	// panels
 	GamePanel m_game_panel;
 	StatusPanel m_status_panel;
 	MessagePanel m_message_panel;
+
+	// systems
+	PlayerSystem m_player_system;
+	PhysicsSystem m_physics_system;
 };
