de3d191a2a10f80c1b94d7ae10c6fd1fc2ea3d07
Akihiro <kevin2006phan@gmail.com>
2025-04-08 02:25:56 +1000

    added infinite world generation

--- src/components/world_component.h
+++ src/components/world_component.h
@@ -2,6 +2,15 @@
 #include <glm/glm.hpp>
 #include "render_component.h"
 
+namespace std {
+	template <>
+	struct hash<glm::ivec2> {
+		std::size_t operator()(const glm::ivec2& v) const noexcept {
+			return std::hash<int>()(v.x) ^ (std::hash<int>()(v.y) << 1);
+		}
+	};
+}
+
 struct Tile {
 	Render render;	
 	bool collidable = false;
@@ -12,7 +21,7 @@ struct Chunk {
 };
 
 struct World {
-	std::unordered_map<int, Chunk> chunks;
-	int chunk_size = 16;
+	std::unordered_map<glm::ivec2, Chunk, std::hash<glm::ivec2>> chunks;
+	glm::ivec2 chunk_size = { 8, 8 };
 	int seed = 1234;
 };
