0f87c633424ce74cfb27053b56490ae8360e5a07
Akihiro <kevin2006phan@gmail.com>
2025-04-11 03:16:43 +1000

    added debugless logging

---
+++ src/logging.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+#ifndef DEBUG
+// assert
+#define AETHEREAL_ASSERT(expr, log) \
+	do { \
+		if (!(expr)) { \
+			std::cerr << "Assertion failed: " << log << std::endl; \
+			std::abort(); \
+		} \
+	} while (0)
+
+// log
+#define AETHEREAL_LOG(msg) \
+	do { \
+		std::cout << msg << std::endl; \
+	} while (0)
+#else
+#define AETHEREAL_ASSERT(expr) \
+	((void)0)
+#define AETHEREAL_LOG(msg) \
+	((void)0)
+#endif
