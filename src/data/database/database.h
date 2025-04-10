8e20c95402378584d639a4139ad1d765ae4beeb4
Akihiro <kevin2006phan@gmail.com>
2025-04-10 19:12:52 +1000

    updated database

--- src/data/database/database.h
+++ src/data/database/database.h
@@ -1,6 +1,18 @@
 #pragma once
+#include "json_parser.h"
+
+// import json type
+using Json = nlohmann::json;
 
 class Database {
 public:
 	Database();
+
+	// loading
+	void load_directory(const std::string& path);
+
+	// data reading
+
+private:
+	std::vector<Json> m_data;
 };
