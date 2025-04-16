67d8d8906dbc762ee76c67546fa3aa6a893ec089
Kevin Phan <kevin2006phan@gmail.com>
2025-04-16 20:37:08 +1000

    added precise collision boxes

--- src/components/collider_component.h
+++ src/components/collider_component.h
@@ -2,5 +2,7 @@
 #include <functional>
 
 struct ColliderComponent {
+	// TODO: Implement per tile collision for larger objects
+	std::vector<bool> collision_tiles;
 	std::function<bool(void)> on_collision;	
 };
