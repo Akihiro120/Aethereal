b0bb58f1dd60773c058541d4d3ac31621b0856a6
Kevin Phan <kevin2006phan@gmail.com>
2025-04-16 17:29:04 +1000

    decoupled game state and added fps counter

--- src/screen/world/message_panel.cpp
+++ src/screen/world/message_panel.cpp
@@ -1,11 +1,21 @@
 #include "message_panel.h"
 
 #include "../utilities/draw_utilities.h"
+#include "../../services/service_locator.h"
+#include <glm/glm.hpp>
+
+MessagePanel::MessagePanel() {
+	// initialize message panel
+	m_game_state = ServiceLocator::get_service<GameState>();	
+}
 
 void MessagePanel::render(const DrawUtils::Box& box) {
 	// message panel
 	terminal_color("white");
 	DrawUtils::draw_box_heavy(box);
+
+	int frame_rate = static_cast<int>(m_game_state->get_frame_time());
+	terminal_print(box.x + 1, box.y + 1, ("FPS: " + std::to_string(frame_rate)).c_str());
 }
 
 void MessagePanel::update() {
--- src/screen/world/message_panel.h
+++ src/screen/world/message_panel.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "../utilities/draw_utilities.h"
+#include "../../game/state/game_state.h"
 
 class MessagePanel {
 public:
-	MessagePanel() = default;
+	MessagePanel();
 
 	void render(const DrawUtils::Box& box);
 	void update();
+
+private:
+	std::shared_ptr<GameState> m_game_state;
 };
