8261bcf0b904ee08b2c577bcebf22f25fbc079db
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-04-19 13:22:49 +1000

    added hotreloading

--- src/data/database/database.h
+++ src/data/database/database.h
@@ -9,6 +9,11 @@
 // import json type
 using Json = nlohmann::json;
 
+struct DatabaseEntry {
+	std::string path;
+	Json json_data;
+};
+
 class Database {
 public:
 	Database() = default;
@@ -22,7 +27,8 @@ public:
 					if (file) {
 						Json json_data;
 						file >> json_data;
-						m_data[entry.path().stem().string()] = json_data;
+						m_data[entry.path().stem().string()].json_data = json_data;
+						m_data[entry.path().stem().string()].path = entry.path().string();
 						AETHEREAL_LOG("Database::load_directory: Loaded JSON file: " << entry.path().string());
 					}
 					else {
@@ -36,10 +42,35 @@ public:
 		}
 	}
 
+	void hot_reload(const std::string& path) {
+		if (m_data.find(path) == m_data.end()) {
+			AETHEREAL_LOG("Database::hot_reload: Path not found in database: " << path);
+			return;
+		}
+
+		std::ifstream file(m_data[path].path);	
+		if (file) {
+			// read the json file
+			Json json_data;
+			try {
+				file >> json_data;
+			} catch (const std::exception& e) {
+				AETHEREAL_LOG("Database::hot_reload: Error parsing JSON file: " << e.what());
+				return;
+			}
+			m_data[path].json_data = json_data;
+			AETHEREAL_LOG("Database::hot_reload: Reloaded JSON file: " << m_data[path].path);
+
+		} else {
+			AETHEREAL_LOG("Database::hot_reload: Failed to open file: " << path);
+		}
+		file.close();
+	}
+
 	Json* get_json(const std::string& path) {
 		auto it = m_data.find(path);
 		if (it != m_data.end()) {
-			return &it->second;
+			return &it->second.json_data;
 		}
 		else {
 			AETHEREAL_LOG("Database::get_json: JSON not found at path: " << path);
@@ -48,5 +79,5 @@ public:
 	}
 
 private:
-	std::unordered_map<std::string, Json> m_data;
+	std::unordered_map<std::string, DatabaseEntry> m_data;
 };
