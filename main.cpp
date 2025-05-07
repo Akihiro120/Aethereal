dcad5cb1598e1d99f61444c8b6d1da8e61773b8d
Kevin Phan <kevin2006phan@gmail.com>
2025-05-07 23:04:10 +1000

    initial

--- main.cpp
+++ main.cpp
@@ -1,40 +1,8 @@
-#include "src/game/game.h"
-#include "src/logging.h"
 #include <iostream>
 
-// define defaults
-#define MAX_SPARSE_SIZE 2000
-#define MAX_COMPONENTS 100
+int main()
+{
+    std::cout << "Hello, World" << std::endl;
 
-#ifdef __arm64__
-#include <csignal>
-#include <ApplicationServices/ApplicationServices.h>
-#endif
-#ifdef _WIN64
-#include <windows.h>
-LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS* pExceptionInfo) {
-	// Handle the exception here
-	// std::cerr << "Unhandled exception occurred! 0x" << pExceptionInfo->ExceptionRecord->ExceptionCode << std::endl;
-	AETHEREAL_LOG("Unhandled exception occurred! 0x" << pExceptionInfo->ExceptionRecord->ExceptionCode);
-	return EXCEPTION_EXECUTE_HANDLER;
-}
-#endif
-
-int main() {
-#ifdef __arm64__
-	// perform arm64 related operations
-	ProcessSerialNumber psn = {0, kCurrentProcess};
-   TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-   SetFrontProcess(&psn);
-#endif
-#ifdef _WIN32
-	// Set up exception handling
-	SetUnhandledExceptionFilter(ExceptionHandler);
-	SetProcessDPIAware();
-#endif
-
-	// begin
-	Game game;
-	game.run();
-	return 0;
+    return 0;
 }
