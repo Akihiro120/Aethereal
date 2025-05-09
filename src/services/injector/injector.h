3c72a6e9a244b3eeb50734aed3b96076d2d4e491
Kevin Phan <kevin2006phan@gmail.com>
2025-05-09 16:43:33 +1000

    added service locator and dep injector

---
+++ src/services/injector/injector.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <tuple>
+#include <memory>
+#include "../service_locator/service_locator.h"
+
+namespace Aethereal::Service
+{
+    template <typename Derived, typename... Deps>
+    class Injector
+    {
+    public:
+        Injector()
+            : m_Dependencies(ServiceLocator::Get<Deps>()...)
+        {
+        }
+
+        template <typename T>
+        std::shared_ptr<T> Get()
+        {
+            return std::get<std::shared_ptr<T>>(m_Dependencies);
+        }
+
+    private:
+        std::tuple<std::shared_ptr<Deps>...> m_Dependencies;
+    };
+}
