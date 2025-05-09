1eb1cc079428f1ba3dee255b72d4ca99fcef4c90
Kevin Phan <kevin2006phan@gmail.com>
2025-05-09 16:43:20 +1000

    added screen manager service

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <cstdlib>
+#include <iostream>
 #include "src/game/game.h"
+
+#include "src/screen/manager/screen_manager.h"
+#include "src/services/service_locator/service_locator.h"
+
+using namespace Aethereal::Screen;
+using namespace Aethereal::Service;
 
 int main()
 {
-    Aethereal::Aethereal aethereal;
-    aethereal.Run();
+    try
+    {
+        // Prepare and setup services
+        ServiceLocator::RegisterService(std::make_shared<ScreenManager>());
+
+        // Begin the game
+        Aethereal::Aethereal aethereal;
+        aethereal.Run();
+    }
+    catch (const std::exception& e)
+    {
+        // Fail
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
