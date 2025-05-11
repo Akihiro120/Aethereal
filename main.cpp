d21a2700688f52c67318667242144dae4a1652b6
Kevin Phan <kevin2006phan@gmail.com>
2025-05-12 01:35:26 +1000

    removed the service registration step

--- main.cpp
+++ main.cpp
@@ -2,19 +2,10 @@
 #include <iostream>
 #include "src/game/game.h"
 
-#include "src/screen/manager/screen_manager.h"
-#include "src/services/service_locator/service_locator.h"
-
-using namespace Aethereal::Screen;
-using namespace Aethereal::Service;
-
 int main()
 {
     try
     {
-        // Prepare and setup services
-        ServiceLocator::RegisterService(std::make_shared<ScreenManager>());
-
         // Begin the game
         Aethereal::Aethereal aethereal;
         aethereal.Run();
