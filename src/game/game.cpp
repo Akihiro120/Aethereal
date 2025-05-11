227a18595b47ee1bfceae69ac2362375f05f3227
Kevin Phan <kevin2006phan@gmail.com>
2025-05-12 01:36:58 +1000

    rremoved legacy code

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,48 +1,31 @@
 #include "game.h"
-#include <chrono>
-#include <thread>
-
-// ftxui
-#include <ftxui/component/component.hpp>
 
 // Aethereal
 #include "../services/service_locator/service_locator.h"
 #include "../screen/manager/screen_manager.h"
+#include "../terminal/terminal.h"
+#include "../screen/screens/main_menu/menu.h"
 
 namespace Aethereal
 {
-    using namespace ftxui;
+    using namespace Aethereal::Screen;
+    using namespace Aethereal::Service;
 
     Aethereal::Aethereal()
-        : m_InteractiveScreen(ScreenInteractive::Fullscreen()),
-          m_ComponentRoot(ConstructSceneRoot()),
-          m_RenderLoop(&m_InteractiveScreen, m_ComponentRoot)
     {
-    }
+        // Prepare and setup services
+        ServiceLocator::RegisterService(std::make_shared<ScreenManager>());
 
-    Component Aethereal::ConstructSceneRoot()
-    {
-        auto sl = Service::ServiceLocator::Get<Screen::ScreenManager>();
+        TerminalCreateInfo info;
+        info.width = 160;
+        info.height = 50;
+        info.fontSize = 24;
+        info.fontPath = "../resources/font/CascadiaCove.ttf";
+        info.title = "Aethereal";
+        Terminal::Open(info);
 
-        // Construct the scene root
-        Component root = Renderer(sl->GetRootComponent(), [&]
-        {
-            return sl->GetRootComponent()->Render();
-        });
-
-        // Define input controls
-        root |= CatchEvent([&](Event e)
-        {
-            if (e == Event::Character('q'))
-            {
-                // Quit the game, close the window
-                m_InteractiveScreen.ExitLoopClosure()();
-                return true;
-            }
-            return false;
-        });
-
-        return root;
+        // set the first screen
+        ServiceLocator::Get<ScreenManager>()->Overlay(std::make_shared<MainMenu::Menu>());
     }
 
     Aethereal::~Aethereal()
@@ -53,34 +36,35 @@ namespace Aethereal
 
     void Aethereal::Run()
     {
-        // Define the frame time
-        const auto frameTime = std::chrono::milliseconds(1000 / 60);
-
         // Render loop.
-        while (m_Running && !m_RenderLoop.HasQuitted())
+        while (Terminal::IsWindowOpen())
         {
             // Progress the game's lifecycle.
             Update();
 
             // Render and display components to screen.
             Render();
-
-            // Delay the main thread for ~16ms
-            std::this_thread::sleep_for(frameTime);
         }
     }
 
     void Aethereal::Render()
     {
-        // Run the rendering once
-        m_RenderLoop.RunOnce();
+        Terminal::Clear();
+        ServiceLocator::Get<ScreenManager>()->Render();
+
+        Terminal::SetForegroundColor(GRAY);
+        Terminal::Print(0, Terminal::GetHeight() - 1, "FPS: " + std::to_string(GetFPS()));
+        Terminal::SetForegroundColor(WHITE);
+        Terminal::Refresh();
     }
 
     void Aethereal::Update()
     {
+        ServiceLocator::Get<ScreenManager>()->Update();
     }
 
     void Aethereal::Clean()
     {
+        Terminal::Close();
     }
 }
