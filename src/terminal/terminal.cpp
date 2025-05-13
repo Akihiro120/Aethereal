be4c65eb5dedadc2f94a70d2d781ffcd223013e5
Kevin Phan <kevin2006phan@gmail.com>
2025-05-14 00:22:15 +1000

    more stuff

--- src/terminal/terminal.cpp
+++ src/terminal/terminal.cpp
@@ -1,52 +1,41 @@
 #include "terminal.h"
-#include <clocale>
 #include <vector>
 #include <string>
-#include <ncpp/NotCurses.hh>
 #include "../utility/logging.h"
 
-static ncpp::NotCurses s_Curses;
-static ncpp::Plane* s_Plane;
+// STATIC MEMBERS
+static int s_ConsoleWidth = 160;
+static int s_ConsoleHeight = 50;
 
-// input
-static std::vector<char> s_PolledInputs;
-
+// IMPLEMENTATION
 namespace Aethereal
 {
     void Terminal::Open()
     {
-        // std::setlocale(LC_ALL, "");
-        Utility::Logging::LOG("Loaded NotCurses Version: " + std::string(s_Curses.version()));
-        s_Curses.s_Plane = s_Curses.get_stdplane();
+        Utility::Logging::LOG("Aethereal Terminal Began Instance");
     }
 
     void Terminal::Close()
     {
-        s_Curses.stop();
     }
 
     void Terminal::Print(int x, int y, const std::string& str)
     {
-        s_Plane->putstr(x, y, str.c_str());
     }
 
     void Terminal::Put(int x, int y, int code)
     {
-        s_Plane->putwch(x, y, (char) code);
     }
 
     void Terminal::Poll()
     {
-        s_PolledInputs.clear();
     }
 
     void Terminal::Clear()
     {
-        s_Plane->erase();
     }
 
     void Terminal::Refresh()
     {
-        s_Curses.render();
     }
 }
