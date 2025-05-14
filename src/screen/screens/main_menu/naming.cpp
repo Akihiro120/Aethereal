71a3fbf2e0bb7de266784871b918de4699e37e3c
Kevin Phan <kevin2006phan@gmail.com>
2025-05-14 23:31:28 +1000

    finished naming

--- src/screen/screens/main_menu/naming.cpp
+++ src/screen/screens/main_menu/naming.cpp
@@ -1,17 +1,50 @@
 #include "naming.h"
-// #include "../../../input/input_handler.h"
-// #include "../../../components/character/name_component.h"
-// #include "../../../components/tags/player_component.h"
+#include "../../../components/character/name_component.h"
+#include "../../../components/tags/player_component.h"
 #include "species.h"
+#include <ftxui/component/component.hpp>
+
+using namespace ftxui;
 
 namespace Aethereal::Screen::MainMenu
 {
     Naming::Naming()
-        : m_Name("")
+        : m_ScreenManager(GetInjection<ScreenManager>()),
+          m_Registry(GetInjection<FECS::Registry>()),
+          m_Name("")
     {
+        ConstructSceneRoot();
     }
 
     ftxui::Component Naming::CreateComponentRoot()
     {
+        auto options = InputOption{
+            .multiline = false,
+            .on_enter = [&]
+        {
+            if (!m_Name.empty())
+            {
+                // attach player and name
+                auto playerID = m_Registry->CreateEntity();
+                m_Registry->Attach(playerID, Components::Tags::PlayerComponent{});
+                m_Registry->Attach(playerID, Components::Character::NameComponent{m_Name});
+
+                // Replace the screen
+                m_ScreenManager->Replace(std::make_shared<Species>());
+            }
+        },
+        };
+        m_NameInput = Input(&m_Name, "Enter your name...", options);
+
+        return Renderer([&]
+        {
+            return vbox({text("Name your character") | bold,
+                         m_NameInput->Render()});
+        });
+    }
+
+    ftxui::Component Naming::CreateComponentContainer()
+    {
+        return m_NameInput;
     }
 }
--- src/screen/screens/main_menu/naming.h
+++ src/screen/screens/main_menu/naming.h
@@ -15,9 +15,14 @@ namespace Aethereal::Screen
             Naming();
 
             virtual ftxui::Component CreateComponentRoot() override;
+            virtual ftxui::Component CreateComponentContainer() override;
 
         private:
             std::string m_Name;
+            ftxui::Component m_NameInput;
+
+            std::shared_ptr<ScreenManager> m_ScreenManager;
+            std::shared_ptr<FECS::Registry> m_Registry;
         };
     }
 }
