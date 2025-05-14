b08a6b43967a025074b275d6b6d6bdad5f7c7fd7
Kevin Phan <kevin2006phan@gmail.com>
2025-05-14 23:31:37 +1000

    finished species

--- src/screen/screens/main_menu/species.h
+++ src/screen/screens/main_menu/species.h
@@ -1,23 +1,48 @@
 #pragma once
 #include "../../screen_base.h"
+#include <fecs/fecs.h>
 #include "../../../services/injector/injector.h"
-#include "../../manager/screen_manager.h"
-#include "../../../database/database.h"
-#include "fecs/registry/registry.h"
+#include "../../../components/tags/player_component.h"
+
+namespace Aethereal::Screen
+{
+    class ScreenManager;
+}
+
+namespace Aethereal
+{
+    class Database;
+}
 
 namespace Aethereal::Screen::MainMenu
 {
+
     class Species : public ScreenBase,
                     public Service::Injector<Species, ScreenManager, FECS::Registry, Database>
     {
     public:
         Species();
 
-        virtual ftxui::Component CreateComponentRoot() override;
+        struct SpeciesMenuSpecification
+        {
+            std::vector<std::string> names;
+            std::vector<std::string> descriptions;
+            std::vector<int> difficulty;
+        };
+
+        virtual ftxui::Component
+        CreateComponentRoot() override;
+        virtual ftxui::Component CreateComponentContainer() override;
 
     private:
-        ScreenManager& m_ScreenManager;
-        Database& m_Database;
-        FECS::Registry& m_ECS;
+        std::shared_ptr<ScreenManager> m_ScreenManager;
+        std::shared_ptr<FECS::Registry> m_Registry;
+        std::shared_ptr<Database> m_Database;
+
+        int m_Selected = 0;
+        ftxui::Component m_SpeciesMenu;
+        SpeciesMenuSpecification m_Specification;
+
+        FECS::View<Components::Tags::PlayerComponent> m_PlayerView;
     };
 }
