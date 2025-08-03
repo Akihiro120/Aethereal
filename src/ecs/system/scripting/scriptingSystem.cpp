24fbd26b6cd51bf072d86f14d0654678e1aad738
Kevin Phan <kevin2006phan@gmail.com>
2025-08-04 04:32:43 +1000

    Refactor ScriptingSystem to create scripting resource
    Replaces the CreateLuaJITCompiler method with logic in Create() to instantiate and attach a ScriptingResource to a new entity. Updates includes and namespaces for consistency and removes unused LuaJIT compiler creation.

--- src/ecs/system/scripting/scriptingSystem.cpp
+++ src/ecs/system/scripting/scriptingSystem.cpp
@@ -1,14 +1,17 @@
 #include "scriptingSystem.h"
-#include <sol/sol.hpp>
-#include "../../ecs.h"
+#include "../../resource/scripting/scriptingResource.h"
+#include "../../../ecs/ecs.h"
 
 namespace Aethereal::ECS::Systems::Scripting
 {
+    using namespace Resource::Scripting;
+
     void ScriptingSystem::Create()
     {
-    }
+        // Create scripting resource
+        FECS::Entity scriptResource = ECS::Registry.CreateEntity();
 
-    void ScriptingSystem::CreateLuaJITCompiler()
-    {
+        ScriptingResourcePtr scriptRes = std::make_shared<ScriptingResource>();
+        ECS::Registry.Attach<ScriptingResourcePtr>(scriptResource, scriptRes);
     }
 }
--- src/ecs/system/scripting/scriptingSystem.h
+++ src/ecs/system/scripting/scriptingSystem.h
@@ -1,16 +1,14 @@
 #pragma once
+#include <string>
 #include "../core/system.h"
 
 namespace Aethereal::ECS::Systems::Scripting
 {
-    class ScriptingSystem : public Core::ISystem
+    class ScriptingSystem : public Aethereal::Core::ISystem
     {
     public:
         ScriptingSystem() = default;
 
         virtual void Create() override;
-
-    private:
-        void CreateLuaJITCompiler();
     };
 }
