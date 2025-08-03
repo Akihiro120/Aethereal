ed8c2f29e5c23fff2434f22eb260bd72bdb92e63
Kevin Phan <kevin2006phan@gmail.com>
2025-08-04 04:32:32 +1000

    Integrate scripting system and display weapon info
    Added the ScriptingSystem to the system manager and included scripting-related headers. The game now retrieves weapon script data and displays its name, description, and rarity using the terminal during drawing.

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2,6 +2,10 @@
 #include <raylib.h>
 #include "../ecs/system/engine/engineSystem.h"
 #include "../ecs/system/rendering/renderSystem.h"
+#include "../ecs/system/scripting/scriptingSystem.h"
+#include "../ecs/ecs.h"
+#include "../ecs/resource/scripting/scriptingResource.h"
+#include "../terminal/terminal.h"
 
 namespace Aethereal
 {
@@ -9,7 +13,8 @@ namespace Aethereal
     {
         // Systems
         m_SystemManager
-            .AttachSystem<ECS::Systems::Core::EngineSystem>()
+            .AttachSystem<ECS::Systems::Engine::EngineSystem>()
+            .AttachSystem<ECS::Systems::Scripting::ScriptingSystem>()
             .AttachSystem<ECS::Systems::Render::RenderSystem>();
 
         // Components
@@ -44,6 +49,17 @@ namespace Aethereal
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        auto scripts = *ECS::Registry.Ctx<ECS::Resource::Scripting::ScriptingResourcePtr>();
+        auto terminal = ECS::Registry.Ctx<Terminal>();
+        if (scripts)
+        {
+            ECS::Resource::Scripting::WeaponScript weapon = scripts->GetWeaponScript("eclipseEdge");
+
+            terminal->Print(0, 0, weapon.information["name"]);
+            terminal->Print(0, 1, weapon.information["description"]);
+            terminal->Print(0, 2, weapon.information["rarity"]);
+        }
 
         m_SystemManager.Draw();
 
